3a3ba8276ae4e0ccdee4de312248a0e241c5a342
akabeko <akabekobeko@gmail.com>
2015-07-29 22:49:01 +0900

    #1 リリース用 ZIP 生成タスクを実装

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,29 @@ gulp.task( 'clean', function( done ) {
   del( [ common.dest ], done );
 } );
 
+// リリース用イメージのビルド
+gulp.task( 'build', [ 'clean', 'stylus' ], function() {
+  // README など
+  gulp.src( [ './README.md', './LICENSE' ] )
+    .pipe( gulp.dest( common.dest ) );
+
+  var src = [
+    common.src + '/stylesheets/application.css',
+    common.src + '/javascripts/theme.js',
+    common.src + '/fonts/**'
+  ];
+
+  return gulp.src( src, { base: common.src } )
+    .pipe( gulp.dest( common.dest ) );
+} );
+
+// リリース用 ZIP イメージ生成
+gulp.task( 'release', [ 'build' ], function() {
+  return gulp.src( [ common.dest + '/**' ], { base: '.' } )
+    .pipe( $.zip( 'minimalflat2.zip' ) )
+    .pipe( gulp.dest( './' ) );
+} );
+
 // ファイル監視
 gulp.task( 'watch', [ 'stylus' ], function() {
   gulp.watch( [ common.src + '/stylus/*.styl' ], [ 'stylus'   ] );
