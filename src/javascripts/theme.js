64639181c573780ccc25d2ee3073e323f04278e2
akabeko <akabekobeko@gmail.com>
2015-08-26 23:25:21 +0900

    #5 bullet_toggle_plus.png と false.png をアイコンフォントに置き換えた

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -41,17 +41,19 @@
     buttonIcon( '.jstb_help',   'js-replace-icon-help' );
 
     // img
-    imageToIcon( 'img[src*="toggle_check.png"]', 'js-replace-icon-check'          );
-    imageToIcon( 'img[src*="true.png"]',         'js-replace-icon-check'          );
-    imageToIcon( 'img[src*="exclamation.png"]',  'js-replace-icon-warning'        );
-    imageToIcon( 'img[src*="delete.png"]',       'js-replace-icon-trash'          );
-    imageToIcon( 'img[src*="add.png"]',          'js-replace-icon-add'            );
-    imageToIcon( 'img[src*="edit.png"]',         'js-replace-icon-edit'           );
-    imageToIcon( 'img[src*="comment.png"]',      'js-replace-icon-comment'        );
-    imageToIcon( 'img[src*="calendar.png"]',     'js-replace-icon-calendar'       );
-    imageToIcon( 'img[src*="2uparrow.png"]',     'js-replace-icon-chevron-top'    );
-    imageToIcon( 'img[src*="1uparrow.png"]',     'js-replace-icon-chevron-up'     );
-    imageToIcon( 'img[src*="1downarrow.png"]',   'js-replace-icon-chevron-down'   );
-    imageToIcon( 'img[src*="2downarrow.png"]',   'js-replace-icon-chevron-bottom' );
+    imageToIcon( 'img[src*="toggle_check.png"]',       'js-replace-icon-check'          );
+    imageToIcon( 'img[src*="true.png"]',               'js-replace-icon-check'          );
+    imageToIcon( 'img[src*="false.png"]',              'js-replace-icon-cross'          );
+    imageToIcon( 'img[src*="exclamation.png"]',        'js-replace-icon-warning'        );
+    imageToIcon( 'img[src*="delete.png"]',             'js-replace-icon-trash'          );
+    imageToIcon( 'img[src*="add.png"]',                'js-replace-icon-add'            );
+    imageToIcon( 'img[src*="edit.png"]',               'js-replace-icon-edit'           );
+    imageToIcon( 'img[src*="comment.png"]',            'js-replace-icon-comment'        );
+    imageToIcon( 'img[src*="calendar.png"]',           'js-replace-icon-calendar'       );
+    imageToIcon( 'img[src*="bullet_toggle_plus.png"]', 'js-replace-icon-toggle-plus'    );
+    imageToIcon( 'img[src*="2uparrow.png"]',           'js-replace-icon-chevron-top'    );
+    imageToIcon( 'img[src*="1uparrow.png"]',           'js-replace-icon-chevron-up'     );
+    imageToIcon( 'img[src*="1downarrow.png"]',         'js-replace-icon-chevron-down'   );
+    imageToIcon( 'img[src*="2downarrow.png"]',         'js-replace-icon-chevron-bottom' );
   } );
 } )( jQuery );
