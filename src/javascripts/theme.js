ef7bf53b459d332be433444e66d6333915f0aded
akabeko <akabekobeko@gmail.com>
2015-08-31 21:46:45 +0900

    #7 プロジェクト一覧をツリー UI 化した プロジェクトのタイトル行をクリックすることでツリーを展開できる。この操作が可能な項目はタイトルの前に +/- アイコンで展開状態を表示する。

--- src/javascripts/theme.js
+++ src/javascripts/theme.js
@@ -1,7 +1,8 @@
 ( function( $ ) {
   if( !( $ ) ) { return; }
 
-  $( document ).ready( function() {
+  // Setup icon
+  function setupIcon() {
     function buttonIcon( target, iconClass ) {
       $( target ).each( function() {
         $( '<i>' ).addClass( iconClass ).prependTo( $( this ) );
@@ -49,11 +50,64 @@
     imageToIcon( 'img[src*="add.png"]',                'js-replace-icon-add'            );
     imageToIcon( 'img[src*="edit.png"]',               'js-replace-icon-edit'           );
     imageToIcon( 'img[src*="comment.png"]',            'js-replace-icon-comment'        );
-    imageToIcon( 'img[src*="calendar.png"]',           'js-replace-icon-calendar'       );
     imageToIcon( 'img[src*="bullet_toggle_plus.png"]', 'js-replace-icon-toggle-plus'    );
     imageToIcon( 'img[src*="2uparrow.png"]',           'js-replace-icon-chevron-top'    );
     imageToIcon( 'img[src*="1uparrow.png"]',           'js-replace-icon-chevron-up'     );
     imageToIcon( 'img[src*="1downarrow.png"]',         'js-replace-icon-chevron-down'   );
     imageToIcon( 'img[src*="2downarrow.png"]',         'js-replace-icon-chevron-bottom' );
+
+    // Pending because there is a jQuery UI problems
+    //imageToIcon( 'img[src*="calendar.png"]', 'js-replace-icon-calendar' );
+  }
+
+  // Tree view ( only projects page )
+  function setupProjectTreeView() {
+    function toggleExpandSymbol( area ) {
+      area.children( 'i.expander' ).each( function() {
+        var expanded = 'expanded';
+        if( $( this ).hasClass( expanded ) ) {
+          $( this ).removeClass( expanded );
+        } else {
+          $( this ).addClass( expanded );
+        }
+      } );
+    }
+
+    $( '.projects.root > li.root > div.root, .projects.root > li.root > ul.projects li.child div.child' ).each( function() {
+      var area = $( this );
+      $( this ).children( 'a.project' ).wrap( '<div class="header">' );
+      $( this ).children( '.header' ).each( function() {
+        // Add expander
+        if( area.next().is( 'ul.projects' ) ) {
+          $( this ).prepend( $( '<i>' ).addClass( 'expander' ) );
+        }
+
+        area.next( 'ul.projects' ).toggle();
+      } )
+      .on( 'click', function() {
+        area.next( 'ul.projects' ).toggle();
+        toggleExpandSymbol( $( this ) );
+      } );
+
+
+
+      /*
+      // Add expander
+      if( $( this ).next().is( 'ul.projects' ) ) {
+        $( this ).prepend( $( '<i>' ).addClass( 'expander' ) );
+      }
+
+      $( this ).next( 'ul.projects' ).toggle();
+      $( this ).on( 'click', function() {
+        $( this ).next( 'ul.projects' ).toggle();
+        toggleExpandSymbol( $( this ) );
+      } )
+      */
+    } );
+  }
+
+  $( document ).ready( function() {
+    setupIcon();
+    setupProjectTreeView();
   } );
 } )( window.jQuery );
